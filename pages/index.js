653e224cceec9e3af5c24768b9ad53d54deb1d8a
Sébastien de Grandpré <1238300+sdegra1975@users.noreply.github.com>
2022-07-04 09:20:47 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,15 @@ export default function Home() {
       <main>
         <img src="/logo1500.png" alt="Logo d'Infonuagix" width="600"/>
         <p className="description">
-          La boîte de développement logiciel infonuagique fait-sur-mesure! <br>
+          La boîte de développement logiciel infonuagique fait-sur-mesure! <br/>
           The custom cloud-native software development shop!
         </p>
         <p className="description">
-          sebastien.degrandpre@infonuagix.com <br>
-          514.942.4431 <br>
-          870 de la Bruyère <br>
-          St-Rémi (Québec) <br>
-          J0L 2L0 <br>
+          sebastien.degrandpre@infonuagix.com <br/>
+          514.942.4431 <br/>
+          870 de la Bruyère <br/>
+          St-Rémi (Québec) <br/>
+          J0L 2L0
         </p>
       </main>
 
