a71f451fa63b57f4b2f4fd367326a835e5514bd0
Sébastien de Grandpré <1238300+sdegra1975@users.noreply.github.com>
2022-07-04 09:31:08 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Infonuagix</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
